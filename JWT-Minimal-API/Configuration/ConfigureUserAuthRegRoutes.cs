1c72ae2c28b9cf211aa35f426faa86a65979fa8b
Rrezakk <52713544+Rrezakk@users.noreply.github.com>
2022-08-30 20:41:59 +0300

    smarter? mapping

---
+++ JWT-Minimal-API/Configuration/ConfigureUserAuthRegRoutes.cs
@@ -0,0 +1,37 @@
+﻿using JWT_Minimal_API.Application.Commands;
+using JWT_Minimal_API.Application.Dtos;
+using JWT_Minimal_API.Application.Models.Db;
+using JWT_Minimal_API.Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_Minimal_API.Configuration
+{
+    public static class UserAuthenticationRegistrationRoutes
+    {
+        public static IEndpointRouteBuilder AddUserEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/user", async (IMediator _mediator, HttpContext _context) =>
+                {
+                    var query = new GetAuthorizedUserQuery(_context);
+                    var response = await _mediator.Send(query);
+
+                    //some mapping
+
+                    return Results.Ok(response);
+                })
+                .Produces<User>(statusCode: 200, contentType: "application/json");
+
+            app.MapPost("/login", [AllowAnonymous] async ([FromBody] UserCredentials user, IMediator _mediator) =>
+                {
+                    var command = new LoginCommand(user);
+                    var result = await _mediator.Send(command);
+                    return result;
+                })
+                .Accepts<UserCredentials>("application/json")
+                .Produces<string>(statusCode: 200, contentType: "application/json");
+            return app;
+        }
+    }
+}
--- JWT-Minimal-API/Program.cs
+++ JWT-Minimal-API/Program.cs
@@ -1,16 +1,11 @@
 using HealthChecks.UI.Client;
-using JWT_Minimal_API.Application.Commands;
-using JWT_Minimal_API.Application.Dtos;
 using JWT_Minimal_API.Application.Mapping;
-using JWT_Minimal_API.Application.Models.Db;
-using JWT_Minimal_API.Application.Queries;
 using JWT_Minimal_API.Application.Services;
 using JWT_Minimal_API.Configuration;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.AspNetCore.Mvc;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -65,25 +60,6 @@ app.MapHealthChecks("/health", new HealthCheckOptions()
 
 app.MapHealthChecksUI().WithMetadata(new AllowAnonymousAttribute());
 
-app.MapGet("/user", async (IMediator _mediator,HttpContext _context) =>
-        {
-            var query = new GetAuthorizedUserQuery(_context);
-            var response = await _mediator.Send(query);
-            
-            //some mapping
-
-            return Results.Ok(response);
-        })
-    .Produces<User>(statusCode: 200, contentType: "application/json");
-
-app.MapPost("/login", [AllowAnonymous] async ([FromBody]UserCredentials user,IMediator _mediator) =>
-    {
-        var command = new LoginCommand(user);
-        var result = await _mediator.Send(command);
-        return result;
-    })
-    .Accepts<UserCredentials>("application/json")
-    .Produces<string>(statusCode: 200, contentType: "application/json");
-
+app.AddUserEndpoints();
 
 app.Run();
